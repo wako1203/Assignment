537ae009c9ee82aa186ae144daf12342a8311134
ㅤWako <hoangthithuynga7071@gmail.com>
2025-01-05 09:09:01 +0700

    Assignment 4

--- magic_8_ball/test/widget_test.dart
+++ magic_8_ball/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:dicee/main.dart';
+import 'package:magic_8_ball/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
